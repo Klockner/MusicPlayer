a7d9b4f0afe5808e2aee501ff83de40bd0ef9900
Gabriel Klockner <gabriel.klockner@ebanx.com>
2017-07-28 17:52:11 -0300

    Add new icons to buttons active effects

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,17 @@
-console.log('\'Allo \'Allo!');
+var btnPlay = document.querySelector('.btn-play');
+
+btnPlay.addEventListener('click', function() {
+  btnPlay.classList.toggle('is-paused');
+});
+
+var btnSound = document.querySelector('.btn-sound');
+
+btnSound.addEventListener('click', function() {
+  btnSound.classList.toggle('is-muted');
+});
+
+var btnShuffle = document.querySelector('.btn-shuffle');
+
+btnShuffle.addEventListener('click', function() {
+  btnShuffle.classList.toggle('is-shuffled');
+});
